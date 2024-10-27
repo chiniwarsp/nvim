dbf9eead48cd665ea880022469b20a9d94e68a27
SantoshChiniwar <chiniwarsp@gmail.com>
2024-10-27 23:56:31 +0800

    mostly NVIM

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ require('lazy').setup {
   require 'plugins.telescope',
   require 'plugins.lsp',
   require 'plugins.autocompletion',
-  require 'plugins.none-ls',
+  require 'plugins.formatting',
   require 'plugins.gitsigns',
   require 'plugins.alpha',
   require 'plugins.indent-blankline',

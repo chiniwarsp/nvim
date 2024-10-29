2812c4074b8046f45adce9b3494ae4466e8f8fdd
SantoshChiniwar <chiniwarsp@gmail.com>
2024-10-29 11:04:01 +0800

    keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,39 +1,29 @@
 -- Set leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
 -- Disable the spacebar key's default behavior in Normal and Visual modes
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
 -- For conciseness
 local opts = { noremap = true, silent = true }
-
 -- save file
 vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
-
 -- save file without auto-formatting
 vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
-
 -- quit file
 vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
-
 -- delete single character without copying into register
 vim.keymap.set('n', 'x', '"_x', opts)
-
 -- Vertical scroll and center
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
 vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
-
 -- Find and center
 vim.keymap.set('n', 'n', 'nzzzv', opts)
 vim.keymap.set('n', 'N', 'Nzzzv', opts)
-
 -- Resize with arrows
 --vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
 --vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
 --vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
 --vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
-
 -- Buffers
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
@@ -75,3 +65,4 @@ vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 vim.api.nvim_set_keymap('n', '<C-b>', '<C-v>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>nh', ':nohlsearch<CR>', { desc = 'Clear search highlights' })
